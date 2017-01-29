5eef93dd014b70754ec834e4649d47b938bdec12
Kees Kluskens <kees@webduck.nl>
2017-01-29 11:40:21 +0100

    Remove html minification

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function HtmlWebpackPlugin (options) {
     inject: true,
     compile: true,
     favicon: false,
-    minify: false,
     cache: true,
     showErrors: true,
     chunks: 'all',
@@ -289,14 +288,6 @@ HtmlWebpackPlugin.prototype.postProcessHtml = function (html, assets, assetTags)
       } else {
         return html;
       }
-    })
-    // Minify
-    .then(function (html) {
-      if (self.options.minify) {
-        var minify = require('html-minifier').minify;
-        return minify(html, self.options.minify);
-      }
-      return html;
     });
 };
 
