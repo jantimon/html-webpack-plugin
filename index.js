485cfe824ae90cc660c1863dc9ce6951398e730d
guang <ylg@meitu.com>
2017-06-01 14:21:27 +0800

    fix: review

--- index.js
+++ index.js
@@ -512,20 +512,16 @@ HtmlWebpackPlugin.prototype.generateAssetTags = function (assets) {
   if (typeof inject === 'function') {
     scripts.forEach(script => {
       if (inject(script.attributes.src)) {
-        console.log('function', 'yes', script.attributes.src);
         body.push(script);
       } else {
-        console.log('function', 'no', script.attributes.src);
         head.push(script);
       }
     });
   } else if (Object.prototype.toString.call(inject) === '[object RegExp]') {
     scripts.forEach(script => {
       if (inject.test(script.attributes.src)) {
-        console.log('regexp', 'yes', script.attributes.src);
         body.push(script);
       } else {
-        console.log('regexp', 'no', script.attributes.src);
         head.push(script);
       }
     });
