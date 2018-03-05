jantimon/html-webpack-plugin

commit 0348d6b48c7313806bc3350ac6516f1ad1bff645

fix(entries): Don't add css entries twice

index.js
@@ -453,12 +453,14 @@ class HtmlWebpackPlugin {
       }
 
       // Webpack outputs an array for each chunk when using sourcemaps
-      // But we need only the entry file
+      // But we need only the initial entry file in case it is a js file
       const entry = chunkFiles[0];
-      assets.chunks[chunkName].size = chunk.size;
-      assets.chunks[chunkName].entry = entry;
-      assets.chunks[chunkName].hash = chunk.hash;
-      assets.js.push(entry);
+      if (/.js($|\?)/.test(entry)) {
+        assets.chunks[chunkName].size = chunk.size;
+        assets.chunks[chunkName].entry = entry;
+        assets.chunks[chunkName].hash = chunk.hash;
+        assets.js.push(entry);
+      }
 
       // Gather all css files
       const css = chunkFiles.filter(chunkFile => /.css($|\?)/.test(chunkFile));

commit 6edc3aa231e40a1c63425e6178a08be807fe5ecf

fix(entries): do not ignore JS if there is also CSS
broken by commit 0348d6b

index.js
@@ -453,13 +453,13 @@ class HtmlWebpackPlugin {
       }
 
       // Webpack outputs an array for each chunk when using sourcemaps
-      // But we need only the initial entry file in case it is a js file
-      const entry = chunkFiles[0];
-      if (/.js($|\?)/.test(entry)) {
+      // or when one chunk hosts js and css simultaneously
+      const js = chunkFiles.find(chunkFile => /.js($|\?)/.test(chunkFile));
+      if (js) {
         assets.chunks[chunkName].size = chunk.size;
-        assets.chunks[chunkName].entry = entry;
+        assets.chunks[chunkName].entry = js;
         assets.chunks[chunkName].hash = chunk.hash;
-        assets.js.push(entry);
+        assets.js.push(js);
       }
 
       // Gather all css files