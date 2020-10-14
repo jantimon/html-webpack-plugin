dd9e37f5a3b6bbaa2fcaa6d209ea4ea3842dd74b
Jan Nicklas <j.nicklas@me.com>
2020-10-14 14:12:47 +0200

    fix: use compiler.hooks.initialize to ensure that the correct compiler configuration is used

--- index.js
+++ index.js
@@ -79,7 +79,10 @@ class HtmlWebpackPlugin {
   }
 
   apply (compiler) {
-    hookIntoCompiler(compiler, this.options, this);
+    // Wait for configuration preset plugions to apply all configure webpack defaults
+    compiler.hooks.initialize.tap('HtmlWebpackPlugin', () => {
+      hookIntoCompiler(compiler, this.options, this);
+    });
   }
 
   /**
