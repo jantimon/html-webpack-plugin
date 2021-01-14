15540a9c1a9546110d164bd70f814237399fe698
Jan Nicklas <j.nicklas@me.com>
2021-01-14 15:23:33 +0100

    chore: update examples to latest webpack version

--- examples/javascript-advanced/dist/webpack-5/bundle.js
+++ examples/javascript-advanced/dist/webpack-5/bundle.js
@@ -1,6 +1,6 @@
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is not neither made for production nor for readable output files.
+ * This devtool is neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
