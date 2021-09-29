f08bd025e34166612226e07213eb40cacf5eacb3
Gabriel Csapo <gabecsapo@gmail.com>
2021-09-29 13:38:56 -0700

    [chore] updates fixtures

--- examples/inline/dist/webpack-5/bundle.js
+++ examples/inline/dist/webpack-5/bundle.js
@@ -19,9 +19,8 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
