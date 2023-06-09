654fc3617e9a027a88ffa11d9b2e8e3cdf1921ad
alexander.akait <sheo13666q@gmail.com>
2023-06-09 20:43:51 +0300

    test: update

--- spec/fixtures/templateParam.js
+++ spec/fixtures/templateParam.js
@@ -1,3 +1,21 @@
 module.exports = function (templateParams) {
+  const version = parseInt(process.version.match(/^v(\d+)/)[1]);
+
+  if (typeof URL !== 'function') {
+    throw new Error('Error');
+  }
+
+  if (typeof URLSearchParams !== 'function') {
+    throw new Error('Error');
+  }
+
+  if (version >= 11 && typeof TextEncoder !== 'function') {
+    throw new Error('Error');
+  }
+
+  if (version >= 11 && typeof TextDecoder !== 'function') {
+    throw new Error('Error');
+  }
+
   return 'templateParams keys: "' + Object.keys(templateParams).join(',') + '"';
 };
