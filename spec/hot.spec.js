353f4994a24b188844cde07862f403b1f2ca0eac
alexander.akait <sheo13666q@gmail.com>
2023-06-21 23:31:18 +0300

    test: emit assets in watch and serve

--- spec/hot.spec.js
+++ spec/hot.spec.js
@@ -115,18 +115,12 @@ describe('HtmlWebpackPluginHMR', () => {
     const templateContent = fs.readFileSync(template, 'utf-8');
     const compiler = new WebpackRecompilationSimulator(webpack(config));
     const jsFileTempPath = compiler.addTestFile(path.join(__dirname, 'fixtures/index.js'));
+    const expected = ["logo.png", "main.js", "favicon.ico", "index.html"];
 
     return compiler.startWatching()
       // Change the template file and compile again
       .then((stats) => {
-        expect(Object.keys(stats.compilation.assets)).toEqual([
-          "logo.png",
-          "main.js",
-          // Special file for testing
-          "spec_fixtures_async_js.js",
-          "favicon.ico",
-          "index.html",
-        ]);
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
         expect(stats.compilation.errors).toEqual([]);
         expect(stats.compilation.warnings).toEqual([]);
 
@@ -135,12 +129,7 @@ describe('HtmlWebpackPluginHMR', () => {
         return compiler.waitForWatchRunComplete();
       })
       .then(stats => {
-        expect(Object.keys(stats.compilation.assets)).toEqual([
-             "main.js",
-             "logo.png",
-             "favicon.ico",
-             "index.html",
-        ]);
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
         expect(stats.compilation.errors).toEqual([]);
         expect(stats.compilation.warnings).toEqual([]);
 
@@ -149,12 +138,68 @@ describe('HtmlWebpackPluginHMR', () => {
         return compiler.waitForWatchRunComplete();
       })
       .then((stats) => {
-        expect(Object.keys(stats.compilation.assets)).toEqual([
-          "logo.png",
-          "main.js",
-          "favicon.ico",
-          "index.html",
-        ]);
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
+        expect(stats.compilation.errors).toEqual([]);
+        expect(stats.compilation.warnings).toEqual([]);
+
+        fs.writeFileSync(template, templateContent);
+      })
+      .then(() => compiler.stopWatching());
+  });
+
+  it('should re-emit favicon and assets from a loader if watch is active and clean enabled', () => {
+    const template = path.join(__dirname, "./fixtures/html-template-with-image.html");
+    const config = {
+      mode: 'development',
+      entry: path.join(__dirname, 'fixtures/index.js'),
+      output: {
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+        path: OUTPUT_DIR
+      },
+      module: {
+        rules: [
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          favicon: path.join(__dirname, "./fixtures/favicon.ico"),
+          template
+        })
+      ]
+    };
+
+    const templateContent = fs.readFileSync(template, 'utf-8');
+    const compiler = new WebpackRecompilationSimulator(webpack(config));
+    const jsFileTempPath = compiler.addTestFile(path.join(__dirname, 'fixtures/index.js'));
+    const expected = ["logo.png", "main.js", "favicon.ico", "index.html"];
+
+    return compiler.startWatching()
+      // Change the template file and compile again
+      .then((stats) => {
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
+        expect(stats.compilation.errors).toEqual([]);
+        expect(stats.compilation.warnings).toEqual([]);
+
+        fs.writeFileSync(jsFileTempPath, 'module.exports = function calc(a, b){ return a - b };');
+
+        return compiler.waitForWatchRunComplete();
+      })
+      .then(stats => {
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
+        expect(stats.compilation.errors).toEqual([]);
+        expect(stats.compilation.warnings).toEqual([]);
+
+        fs.writeFileSync(template, templateContent.replace(/Some unique text/, 'Some other unique text'));
+
+        return compiler.waitForWatchRunComplete();
+      })
+      .then((stats) => {
+        expect(expected.every(val => Object.keys(stats.compilation.assets).includes(val))).toBe(true);
         expect(stats.compilation.errors).toEqual([]);
         expect(stats.compilation.warnings).toEqual([]);
 
